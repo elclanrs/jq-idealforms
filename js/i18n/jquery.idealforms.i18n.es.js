3767e075185e00791314688255af10dee06eba3c
elclanrs <elclanrs@gmail.com>
2013-10-09 20:47:38 -0400

    ES localization file

---
+++ js/i18n/jquery.idealforms.i18n.es.js
@@ -0,0 +1,39 @@
+/**
+ * i18n ES
+ */
+$.idealforms.i18n.es = {
+
+  customInputs: {
+    open: 'Abrir'
+  },
+
+  steps: {
+    step: 'Paso'
+  },
+
+  errors: {
+    required: 'Este campo es obligatorio',
+    digits: 'Debe contener sólo cifras',
+    name: 'Debe contener al menos 3 caracteres y contener únicamente letras',
+    email: 'Debe ser una dirección de correo electrónico válida',
+    username: 'Debe contener entre 4 y 32 caracteres y empezar con una letra. Puedes usar letras, números, guiones, y un punto',
+    pass: 'Debe contener al menos 6 caracteres y contener al menos un número, una letra mayúscula y una minúscula',
+    strongpass: 'Debe contener al menos 8 caracteres y contener al menos una letra mayúscula, una letra minúscula, y un número o caracter especial',
+    phone: 'Debe ser un número te teléfono válido',
+    zip: 'Debe ser un código postal válido',
+    url: 'Debe ser una dirección web válida',
+    number: 'Debe ser un número',
+    range: 'Debe ser un número entre {0} and {1}.',
+    min: 'Debe contener al menos {0} caracteres',
+    max: 'No puede contener más de {0} caracteres',
+    minoption: 'Selecciona al menos {0} opciones',
+    maxoption: 'No puede haber más de {0} opciones seleccionadas',
+    minmax: 'Debe contener al menos {0} pero no más de {1} caracteres',
+    select: 'Selecciona una option',
+    extension: 'Los ficheros deben tener una extensión válida',
+    equalto: 'Debe ser idéntico a <strong>"{0}"</strong>',
+    date: 'Debe ser una fecha válida {0}',
+    ajax: 'Cargando...'
+  }
+
+};
