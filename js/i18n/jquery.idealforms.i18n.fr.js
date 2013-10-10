27f32fe1b91b528209a260de27e6847daacb4146
elclanrs <elclanrs@gmail.com>
2013-10-10 18:09:12 -0400

    seamless localization + FR locale

---
+++ js/i18n/jquery.idealforms.i18n.fr.js
@@ -0,0 +1,40 @@
+/**
+ * i18n FR
+ */
+$.idealforms.i18n.fr = {
+
+  customInputs: {
+    open: 'Ouvrir'
+  },
+
+  steps: {
+    step: 'Étape'
+  },
+
+  errors: {
+
+    required: 'Ce champ est obligatoire',
+    digits: 'Veuillez fournir seulement des chiffres',
+    name: 'Veuillez fournir au moins 3 caractères et seulment des lettres',
+    email: 'Veuillez fournir une adresse électronique valide',
+    username: 'Veuillez fournir entre 4 et 32 caractères et commencer par une lettre. Vous pouvez utiliser des lettres, des numéros, des tirets et un point.',
+    pass: 'Veuillez fournir au moins 6 caractères, un numéro, une lettre majuscule et une minuscule',
+    strongpass: 'Veuillez fournir au moins 8 caractères, une lettre majuscule, une minuscule et une numero ou caractère special',
+    phone: 'Veuillez fournir une numéro de téléphone valide',
+    zip: 'Veuillez fournir un code postal',
+    url: 'Veuillez fournir une adresse URL valide',
+    number: 'Veuillez fournir un numéro valide',
+    range: 'Veuillez fournir un numéro entre {0} et {1}',
+    min: 'Veuillez fournir au moins {0} caractères',
+    max: 'Veuillez fournir au plus {0} caractères',
+    minoption: 'Veuillez sélectionné au moins {0 options',
+    maxoption: 'Veuillez sélectionné au plus {0} options',
+    minmax: 'Veuillez fournir entre {0} et {1} caractères',
+    select: 'Sélectionne une option',
+    extension: 'Veuillez fournir des fichiers avec une extension valide ({*})',
+    equalto: 'Veuillez fournir encore la même valeur que {0}',
+    date: 'Veuillez fournir une date valide {0}',
+    ajax: 'Chargement en cours...'
+  }
+
+};
