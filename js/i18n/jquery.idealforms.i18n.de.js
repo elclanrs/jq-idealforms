d1ad12cb45956d8c23c242e464340af551ce42b3
elclanrs <elclanrs@gmail.com>
2013-12-05 17:14:15 -0500

    i18n DE #23

---
+++ js/i18n/jquery.idealforms.i18n.de.js
@@ -0,0 +1,39 @@
+/**
+ * i18n DE
+ */
+$.idealforms.i18n.de = {
+
+  customInputs: {
+    open: 'Öffnen'
+  },
+
+  steps: {
+    step: 'Schritt'
+  },
+
+  errors: {
+    required: 'Dieses Feld ist erforderlich.',
+    digits: 'Bitte nur Zahlen eingeben.',
+    name: 'Bitte mindestens 3 Buchstaben/ Zeichen eingeben.',
+    email: 'Bitte eine gültige E-Mail Adresse eingeben. (Beispiel: user@gmail.com)',
+    username: 'Der Name muss aus mindestens 4 Zeichen bestehen und darf 32 Zeichen nicht überschreiten. Erlaubt sind Buchstaben, Zahlen, Bindestriche und Punkte.',
+    pass: 'Das Passwort muss mindestens aus 6 Zeichen bestehen. Es muss aus Groß- und Kleinbuchstaben, sowie Zahlen enthalten.',
+    strongpass: 'Das Passwort muss aus mindestens 8 Zeichen bestehen. Das Passwort muss Groß- und Kleinbuchstaben, sowie aus Zahlen und Sonderzeichen enthalten.',
+    phone: 'Bitte eine gültige Telefonnummer eingeben. (Beispiel: 555-123-4567)',
+    zip: 'Bitte eine gültige PLZ eingeben. (Beispiel: 33245)',
+    url: 'Bitte eine gültige URL eingeben. (Beispiel: www.google.com)',
+    number: 'Bitte eine gültige Zahl eingeben.',
+    range: 'Bitte eine Zahl zwischen {0} und {1} eingeben',
+    min: 'Bitte mindestens <strong>{0}</strong> Zeichen eintragen.',
+    max: 'Bitte nicht mehr als <strong>{0}</strong> Zeichen eingeben.',
+    minOption: 'Bitte mindestens <strong>{0}</strong> Option/en wählen.',
+    maxOption: 'Bitte nicht mehr als <strong>{0}</strong> Option/en wählen.',
+    minmax: 'Bitte zwischen {0} und {1} Zeichen eintragen.',
+    select: 'Bitte eine Option auswählen.',
+    extension: 'Bitte einen gültigen Dateinamen eingeben. (Beispiel: "{0}")',
+    equalto: 'Die Eingabe muss mit <strong>"{0}"</strong> gleich sein.',
+    date: 'Bitte ein gültiges Datum eingeben. (Beispiel: {0})',
+    ajax: 'Ladevorgang ...'
+  }
+
+};
